95d9a8926b16f81eb386c62392054c6d7f8aa0b4
Emily Su <emilysu822@gmail.com>
2021-07-07 15:37:39 -0700

    Added log out feature

--- app/src/main/java/com/example/instagram/MainActivity.java
+++ app/src/main/java/com/example/instagram/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import com.parse.ParseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu (adds items to the action bar if it's present)
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        // Must return true for the menu to be displayed
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Log out
+        if (item.getItemId() == R.id.miLogout) {
+            ParseUser.logOut();
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
